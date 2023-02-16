ec83159570dbaf3876ac3ec77b0cf9199dabe763
Adrian2305 <rocadavid97@gmail.com>
2023-02-16 15:58:47 -0500

    Subiendo el la practica

---
+++ src/components/user/controller.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from "express";
+import { PrismaClient} from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const findAll = async(req:Request, res:Response): Promise<void> => {
+    try{
+        const users = await prisma.user.findMany();
+
+        res.status(200).json({
+            ok:true,
+            data:users
+        })
+
+    }catch(error){
+        res.status(500).json({
+            ok:false,
+            message:error
+        })
+    }
+}
+
+export const store = async(req:Request, res:Response):Promise<void> => {
+    try{
+        const data = req.body;
+        await prisma.user.create({data});
+        res.status(201).json({
+            ok:true,
+            message:"Usuario creado correctamente"
+        });
+    }catch(error){
+        res.status(500).json({
+            ok:false,
+            message:error
+        })
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import app from "./app"
+const PORT = 9002
+app.listen(PORT, () => console.log(`Server init at http://localhost:${PORT}`));
